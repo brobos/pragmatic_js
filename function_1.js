5f28fecf9d986d8043d2db78887dee80bfe81288
Albena <albena_7@abv.bg>
2016-06-23 20:30:34 +0300

    Update function_1.js

--- function_1.js
+++ function_1.js
@@ -1,23 +1,7 @@
-function xyz(a) {
-	console.log(a);
-	a.z = 10;
+function test(a) {
+	return a;
+
 }
 
-var a = {z: 5}
 
-xyz(a);
-console.log(a);
-
-
-
-
-
-function xyz(a) {
-	console.log(a);
-	a.z = "test";
-}
-
-var a = "original value"
-
-xyz(a);
-console.log(a);
+test(10);
